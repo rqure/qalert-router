1417a1734173a909d99478a5872a88c5fd84a827
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-12-22 18:40:01 +0000

    Fixing nesting of multibinding

--- alert_worker.go
+++ alert_worker.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 
-	qdb "github.com/rqure/qdb/src"
 	"github.com/rqure/qlib/pkg/app"
 	"github.com/rqure/qlib/pkg/data"
+	"github.com/rqure/qlib/pkg/data/binding"
 	"github.com/rqure/qlib/pkg/data/notification"
 	"github.com/rqure/qlib/pkg/data/query"
 	"github.com/rqure/qlib/pkg/log"
@@ -19,28 +19,28 @@ type AlertWorker struct {
 
 func NewAlertWorker(store data.Store) *AlertWorker {
 	return &AlertWorker{
-		db:                 db,
+		store:              store,
 		isLeader:           false,
 		notificationTokens: []data.NotificationToken{},
 	}
 }
 
-func (w *AlertWorker) OnBecameLeader(context.Context) {
+func (w *AlertWorker) OnBecameLeader(ctx context.Context) {
 	w.isLeader = true
 
 	w.notificationTokens = append(w.notificationTokens, w.store.Notify(
-ctx,
-notification.NewConfig().
-SetEntityType(        "AlertController").
-SetFieldName(        "SendTrigger"),
-	}, notification.NewCallback(w.ProcessNotification)))
+		ctx,
+		notification.NewConfig().
+			SetEntityType("AlertController").
+			SetFieldName("SendTrigger"),
+		notification.NewCallback(w.ProcessNotification)))
 }
 
-func (w *AlertWorker) OnLostLeadership(context.Context) {
+func (w *AlertWorker) OnLostLeadership(ctx context.Context) {
 	w.isLeader = false
 
 	for _, token := range w.notificationTokens {
-		token.Unbind()
+		token.Unbind(ctx)
 	}
 
 	w.notificationTokens = []data.NotificationToken{}
@@ -58,25 +58,24 @@ func (w *AlertWorker) DoWork(context.Context) {
 
 }
 
-func (w *AlertWorker) ProcessNotification(ctx context.Context, notification data.Notification) {
+func (w *AlertWorker) ProcessNotification(ctx context.Context, n data.Notification) {
 	if !w.isLeader {
 		return
 	}
 
-	log.Info("Received notification: %v", notification)
+	log.Info("Received notification: %v", n)
 
-	applicationName := notification.GetContext(0).GetValue().GetString()
-	description := notification.GetContext(1).GetValue().GetString()
-	ttsAlert := notification.GetContext(2).GetValue().GetBool()
-	emailAlert := notification.GetContext(3).GetValue().GetBool()
+	applicationName := n.GetContext(0).GetValue().GetString()
+	description := n.GetContext(1).GetValue().GetString()
+	ttsAlert := n.GetContext(2).GetValue().GetBool()
+	emailAlert := n.GetContext(3).GetValue().GetBool()
 
 	if ttsAlert {
 		log.Info("Sending TTS alert: %v", description)
 
-		controllers := query.New(w.store).Find(qdb.SearchCriteria{
-			EntityType: "AudioController",
-			Conditions: []qdb.FieldConditionEval{},
-		})
+		controllers := query.New(w.store).
+			ForType("AudioController").
+			Execute(ctx)
 
 		for _, controller := range controllers {
 			controller.GetField("TextToSpeech").WriteString(ctx, description)
@@ -86,30 +85,18 @@ func (w *AlertWorker) ProcessNotification(ctx context.Context, notification data
 	if emailAlert {
 		log.Info("Sending email alert: %v", description)
 
-		controllers := query.New(w.store).Find(qdb.SearchCriteria{
-			EntityType: "SmtpController",
-			Conditions: []qdb.FieldConditionEval{},
-		})
+		multi := binding.NewMulti(w.store)
+		controllers := query.New(multi).
+			ForType("SmtpController").
+			Execute(ctx)
 
 		for _, controller := range controllers {
 			// Needs to be written as an atomic bulk operation so notifications don't get mingled together
-			w.store.Write([]*qdb.DatabaseRequest{
-				{
-					Id:    controller.GetId(),
-					Field: "Subject",
-					Value: qdb.NewStringValue("Alert from " + applicationName + " service"),
-				},
-				{
-					Id:    controller.GetId(),
-					Field: "Body",
-					Value: qdb.NewStringValue(description),
-				},
-				{
-					Id:    controller.GetId(),
-					Field: "SendTrigger",
-					Value: qdb.NewIntValue(0),
-				},
-			})
+			controller.GetField("Subject").WriteString(ctx, "Alert from "+applicationName+" service")
+			controller.GetField("Body").WriteString(ctx, description)
+			controller.GetField("SendTrigger").WriteInt(ctx)
 		}
+
+		multi.Commit(ctx)
 	}
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 
-	qdb "github.com/rqure/qdb/src"
 	"github.com/rqure/qlib/pkg/app"
 	"github.com/rqure/qlib/pkg/app/workers"
 	"github.com/rqure/qlib/pkg/data/store"
@@ -19,13 +18,13 @@ func getDatabaseAddress() string {
 }
 
 func main() {
-	db := store.NewWeb(store.WebConfig{
+	s := store.NewWeb(store.WebConfig{
 		Address: getDatabaseAddress(),
 	})
 
-	storeWorker := workers.NewStore(db)
-	leadershipWorker := workers.NewLeadership(db)
-	alertWorker := NewAlertWorker(db)
+	storeWorker := workers.NewStore(s)
+	leadershipWorker := workers.NewLeadership(s)
+	alertWorker := NewAlertWorker(s)
 	schemaValidator := leadershipWorker.GetEntityFieldValidator()
 
 	schemaValidator.RegisterEntityFields("Root", "SchemaUpdateTrigger")
@@ -37,17 +36,9 @@ func main() {
 	leadershipWorker.BecameLeader().Connect(alertWorker.OnBecameLeader)
 	leadershipWorker.LosingLeadership().Connect(alertWorker.OnLostLeadership)
 
-	// Create a new application configuration
-	config := qdb.ApplicationConfig{
-		Name: "alert",
-		Workers: []qdb.IWorker{
-			storeWorker,
-			leadershipWorker,
-			alertWorker,
-		},
-	}
-
-	app := app.NewApplication(config)
-
-	app.Execute()
+	a := app.NewApplication("alert")
+	a.AddWorker(storeWorker)
+	a.AddWorker(leadershipWorker)
+	a.AddWorker(alertWorker)
+	a.Execute()
 }
